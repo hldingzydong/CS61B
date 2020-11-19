9909df41c4da1c80bbc5dc7d2a062396ca097eb5
hldingzydong <847394047@qq.com>
2020-11-20 00:00:56 +0800

    clab8 half done

--- clab8/FlightSolver.java
+++ clab8/FlightSolver.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
@@ -8,13 +10,38 @@ import java.util.ArrayList;
  */
 public class FlightSolver {
 
+    private PriorityQueue<Flight> pq;
+
     public FlightSolver(ArrayList<Flight> flights) {
-        /* FIX ME */
+        Comparator<Flight> flightComparator = Comparator.comparingInt(flight -> flight.startTime);
+        pq = new PriorityQueue<Flight>(flightComparator);
+        for(Flight flight : flights) {
+            pq.add(flight);
+        }
     }
 
     public int solve() {
-        /* FIX ME */
-        return -1;
+        int max = 0;
+        while (pq.size() > 1) {
+            Flight flight1 = pq.poll();
+            Flight flight2 = pq.peek();
+            if(flight1.endTime <= flight2.startTime) {
+                max = Math.max(max, flight1.passengers);
+            } else if (flight1.endTime > flight2.endTime) {
+                flight2 = pq.poll();
+                Flight flight = new Flight(flight2.startTime, flight2.endTime, flight1.passengers + flight2.passengers);
+                pq.add(flight);
+                flight = new Flight(flight2.endTime, flight1.endTime, flight1.passengers);
+                pq.add(flight);
+            } else {
+                flight2 = pq.poll();
+                Flight flight = new Flight(flight2.startTime, flight1.endTime, flight1.passengers + flight2.passengers);
+                pq.add(flight);
+                flight = new Flight(flight1.endTime, flight2.endTime, flight2.passengers);
+                pq.add(flight);
+            }
+        }
+        return Math.max(max, pq.peek().passengers);
     }
 
 }
